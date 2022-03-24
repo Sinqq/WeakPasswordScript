543d0e0d8bc6e94c6f7da151f2bd9b0165e21437
Sinqq <100397624+Sinqq@users.noreply.github.com>
2022-03-24 07:21:32 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,14 +1,23 @@
 
 print "Set up new password:"
- a = gets.chomp
+ a = gets.CHOMP
 print "One more time for confirmation:"
  b = gets.chomp
 if b != a then
  #If the verification is incorrect. 
   print "Passwords don't seem to match, try again:"
   c = gets.chomp
-  sleep(3)
+
   print "One more time for confirmation:"
   d = gets.chomp
+ #If password is sucessfully saved
 else 
-  
+  puts "OK, Saved!"
+ sleep (2)
+print "Login:"
+ e = gets.chomp
+ if e != a
+  then print "Incorrect! Please try again:"
+   f = gets.chomp
+    if f != a then print "You typo too much. Wait a minute now"
+     sleep (60)
